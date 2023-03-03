69ddc7141c9d876680c35da49797e8daced52327
George Pegias <47457023+gapegias@users.noreply.github.com>
2023-03-03 17:36:47 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-   production: false,
+   production: true,
    apiTrainers:"https://zt-lost-in-translation-api-production.up.railway.app/trainers",
    apiTrainersKey:"byV2uhRBjfINbw5LdXUOFpwexXkndJIow08oEF91sqRobUjpVxlhtzp9M9yb2bt6",
    apiPokemon:"https://pokeapi.co/api/v2/pokemon"

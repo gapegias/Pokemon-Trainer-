94342be49d3d348dffd58e29f691ff136a35cf48
George Pegias <47457023+gapegias@users.noreply.github.com>
2023-03-03 17:28:39 +0200

    Delete environments directory

--- environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiTrainers:"https://zt-lost-in-translation-api-production.up.railway.app/trainers",
-  apiTrainersKey:"byV2uhRBjfINbw5LdXUOFpwexXkndJIow08oEF91sqRobUjpVxlhtzp9M9yb2bt6",
-  apiPokemon:"https://pokeapi.co/api/v2/pokemon"
-};
